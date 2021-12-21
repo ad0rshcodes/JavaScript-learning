e2d55c5510934e0543df8afb887b41e097d216eb
adarsh-sharma6218 <adarshsharma6218@gmail.com>
2021-12-21 16:43:44 +0530

    feat: counter project update

---
+++ Counter/script.js
@@ -0,0 +1,14 @@
+counter = () => {
+  let current = parseInt(
+    document.getElementsByClassName("counter")[0].innerHTML
+  );
+
+  console.log(current);
+  if (current < 1000) {
+    current = current + 1;
+  }
+
+  document.getElementsByClassName("counter")[0].innerHTML = current;
+};
+
+setInterval(counter, 25);
